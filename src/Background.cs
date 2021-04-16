9888c53d0452b3ef5017b32ed1115f578abda2ad
copygirl <copygirl@mcft.net>
2021-04-16 18:43:43 +0200

    Fix viewport not moving when paused

--- src/Background.cs
+++ src/Background.cs
@@ -7,8 +7,8 @@ public class Background : TextureRect
         var offset       = new Vector2(8, 8);
         var tileSize     = Texture.GetSize();
         var viewportSize = GetViewport().Size;
-        var camera       = LocalPlayer.Instance.GetNode<Camera2D>("Camera");
-        RectPosition = ((camera.GlobalPosition - viewportSize / 2) / tileSize).Floor() * tileSize - offset;
+        var cameraPos    = LocalPlayer.Instance.GetNode<Camera2D>("Camera").GetCameraPosition();
+        RectPosition = ((cameraPos - viewportSize / 2) / tileSize).Floor() * tileSize - offset;
         RectSize     = ((viewportSize + offset) / tileSize + Vector2.One).Ceil() * tileSize;
     }
 }
